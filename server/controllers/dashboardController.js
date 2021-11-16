84d0512a50c3a1a82d717c5487be4c6afd3f5179
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-11-16 09:37:38 -0800

    Added some new middlewares, still need to add more.

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -27,11 +27,101 @@ dashboardController.postYearlyGoal = function (req, res, next) {
 
   db.query(query.text, query.params)
     .then(() => {
-      res.locals.queryStatus = 'Sucussfully posted yearly goal to DB.';
+      res.locals.queryStatus = 'Sucessfully posted yearly goal to DB.';
       return next();
     })
-    .catch()
+    .catch(() => {
+      console.log('Error in dashboardController.postYearlyGoal while querying database.');
+      next({
+        log: 'Error in dashboardController.postYearlyGoal while querying database.',
+        status: 500,
+        message: 'Error in dashboardController.postYearlyGoal while querying database.'
+      })
+    })
 }
+
+/**
+ * Get the current donation amount that the user has made this year.
+ * @param {*} req takes user ID
+ * @param {*} res
+ * @param {*} next
+ * @returns number of the current donation sum.
+ */
+dashboardController.getCurrentYearlyDonations = function (req, res, next) {
+  const { userId } = req.body;
+
+  if (!userId) {
+    console.log('Unsuccessful get of current yearly donations in dashboardController.getCurrentYearlyDonations, missing userId.');
+    res.locals.queryStatus = 'Unsuccessful get of current yearly donations in dashboardController.getCurrentYearlyDonations, missing userId.';
+    return next();
+  }
+
+  const query = {
+    params: [userId],
+    text: `SELECT current_amount
+          FROM yearly_goals
+          WHERE user_id = $1 AND year = DATE_PART('YEAR', CURRENT_TIMESTAMP);`
+  }
+
+  db.query(query.text, query.params)
+    .then((dbRes) => {
+      if (dbRes.rows.length >= 1) {
+        res.locals.queryStatus = 'Sucussfully got current yearly donations from DB.';
+        res.locals.currentYearlyDonations = dbRes.rows[0].current_amount;
+      } else {
+        res.locals.queryStatus = 'Unsuccessfully attempt to get current yearly donations from DB.';
+      }
+      return next();
+    })
+    .catch(() => {
+      console.log('Error in dashboardController.getCurrentYearlyDonations while querying database.');
+      next({
+        log: 'Error in dashboardController.getCurrentYearlyDonations while querying database.',
+        status: 500,
+        message: 'Error in dashboardController.getCurrentYearlyDonations while querying database.'
+      })
+    })
+}
+
+/**
+ * Post donations to the database AND update the yearly table sum count
+ * @param {*} req userid, donation amount, payee, memo, category
+ * @param {*} res nothing
+ * @param {*} next
+ */
+dashboardController.postContribution = function(req, res, next) {
+  if (!req.body.userId || !req.body.payee || !req.body.category || !req.body.amount) {
+    console.log('Unsuccessful post of contribution in dashboardController.postContribution, missing information');
+    res.locals.queryStatus = 'Unsuccessful post of contribution in dashboardController.postContribution, missing information';
+  }
+
+  const query = {
+    params: [req.body.userId, req.body.payee, req.body.category, req.body.amount, req.body.memo],
+    text: `INSERT INTO contributions (user_id, payee, category, amount, memo) VALUES ($1, $2, $3, $4, $5);
+    UPDATE yearly_goals SET current_amount = (SELECT SUM(amount) AS newSum FROM contributions GROUP BY user_id, year HAVING user_id = $1 AND year = DATE_PART('YEAR', CURRENT_DATE)) WHERE user_id = $1;`
+  }
+
+  db.query(query.text, query.params)
+    .then((dbRes) => {
+      if (dbRes.rows.length >= 1) {
+        res.locals.queryStatus = 'Successfully retrieved updated contribution sum for user in postContribution.';
+      } else {
+        console.log('Unsuccessful attempt to insert into contributions or update yearly_goals in dashboardController.postContribution');
+        res.locals.queryStatus = 'Unsuccessful attempt to insert into contributions or update yearly_goals in dashboardController.postContribution';
+      }
+    })
+    .catch(() => {
+      console.log('Error in dashboardController.postContribution while querying database.');
+      next({
+        log: 'Error in dashboardController.postContribution while querying database.',
+        status: 500,
+        message: 'Error in dashboardController.postContribution while querying database.'
+      })
+    })
+
+
+}
+
 
 /**
  *
