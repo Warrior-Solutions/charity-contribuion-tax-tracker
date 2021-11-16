868bd04afb94783094e5c4fb2708369559d7dc85
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-11-15 21:19:59 -0800

    Removed a sql query comment and replaced it in another file.

--- server/model/model.js
+++ server/model/model.js
@@ -1,44 +1,11 @@
 // Import PG DB.
 const { Pool } = require('pg');
 
-/** @construct
- *
- *CREATE TABLE users (
-  _id SERIAL UNIQUE PRIMARY KEY,
-  email varchar(255) NOT NULL,
-  password varchar(255),
-  oauth_id varchar(255)
-  );
-
- *CREATE TABLE contributions (
-  _id SERIAL UNIQUE PRIMARY KEY,
-  amount NUMERIC NOT NULL,
-  payee varchar(255) NOT NULL,
-  category varchar(255) NOT NULL,
-  donated_at TIME DEFAULT CURRENT_TIMESTAMP,
-  user_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(_id);
-
-  );
- *
- *CREATE TABLE yearly_goals (
-  _id SERIAL UNIQUE PRIMARY KEY,
-  goal_amount NUMERIC,
-  current_amount NUMERIC,
-  user_id int NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(_id);
-  );
- *
- *
- *
-**/
-
 // Attach connection URI.
 const myURI = 'postgres://mzwmrclg:XHg58ZPeu-cAJ0APePTIfGYNJSwrg84S@kashin.db.elephantsql.com/mzwmrclg';
 
 // Connect to DB.
 const db = new Pool({ connectionString: myURI });
-
 
 // Export query functionality.
 module.exports = {
