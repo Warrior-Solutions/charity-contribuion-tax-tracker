222023bbbdd8dcd73779ee30de277c87600c02af
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-11-15 21:16:02 -0800

    Added comments to model.js

--- server/model/model.js
+++ server/model/model.js
@@ -1,3 +1,4 @@
+// Import PG DB.
 const { Pool } = require('pg');
 
 /** @construct
@@ -32,11 +33,14 @@ const { Pool } = require('pg');
  *
 **/
 
+// Attach connection URI.
 const myURI = 'postgres://mzwmrclg:XHg58ZPeu-cAJ0APePTIfGYNJSwrg84S@kashin.db.elephantsql.com/mzwmrclg';
 
-
+// Connect to DB.
 const db = new Pool({ connectionString: myURI });
 
+
+// Export query functionality.
 module.exports = {
   query: async (text, params, callback) => {
     console.log(`🚀🚀 SQL queried with text🚀: ${text} and params🚀: ${params}`);
