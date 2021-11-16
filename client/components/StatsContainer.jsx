8c3d68a9a68bfb0b33b5e1ca0e603cf784121f38
klintonkuach <clintonquach@gmail.com>
2021-11-16 15:03:17 -0800

    dashboard layout somewhat done

--- client/components/StatsContainer.jsx
+++ client/components/StatsContainer.jsx
@@ -3,12 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 const StatsContainer = (props) => {
   return (
+    <div>
     <div className="statsContainer">
-      <div>
+      <div id="contGoal">
       <div className="cont">Your Yearly Contribution:</div>
       <div className="goal">Your Yearly Goal:</div>
       </div>
-      <div id="pie"></div>
+      <div className="pie">PIE</div>
+    <div className="graph">GRAPH</div>
+    
+      </div>
+   
     </div>
   );
 };
