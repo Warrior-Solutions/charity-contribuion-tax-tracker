35bc382f7bf251ac1765e3fee800952469ec926c
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-11-17 13:20:10 -0800

    got a pie chart and a line chart up. Not yet styled.

--- client/components/LineChart.jsx
+++ client/components/LineChart.jsx
@@ -1,5 +1,15 @@
 import React, {Component} from 'react';
-import {Bar, Line, Pie} from 'react-chartjs-2';
+// import {Bar, Line, Pie} from 'react-chartjs-2';
+import {
+  // main component
+  Chart,
+  // graphs
+  Bars, Cloud, Dots, Labels, Lines, Pies, RadialLines, Ticks, Title,
+  // wrappers
+  Layer, Animate, Transform, Handlers,
+  // helpers
+  DropShadow, Gradient
+} from 'rumble-charts';
 
 class LineChartContainer extends Component{
   constructor(props){
@@ -7,12 +17,69 @@ class LineChartContainer extends Component{
     this.state = {
       chartData:props.chartData
     }
+
+
+    const series = [
+      {
+        data: [
+          1,
+          2,
+          3
+        ]
+      },
+      {
+        data: [
+          5,
+          7,
+          11
+        ]
+      },
+      {
+        data: [
+          13,
+          17,
+          19
+        ]
+      }
+    ];
   }
+
+
 
   render(){
     return (
       <div className="chart">
         <h1> LINE CHART GOES HERE</h1>
+        <Chart
+        height={300}
+        minY={0}
+        series={[
+          {
+            data: [
+              1,
+              2,
+              3
+            ]
+          },
+          {
+            data: [
+              5,
+              7,
+              11
+            ]
+          },
+          {
+            data: [
+              13,
+              17,
+              19
+            ]
+          }
+        ]}
+        width={600}
+      >
+        <Lines />
+      </Chart>
       </div>
     )
   }
